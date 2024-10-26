15e5b0a1344204b552c8bc1858501fd50670aa43
kauanuness <kaua.souza@sptech.school>
2024-10-26 00:42:52 -0300

    arrumando url de subcategorias

--- src/hooks/api/subCategoriasApi.js
+++ src/hooks/api/subCategoriasApi.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const getSubcategorias = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/subcategorias"); // Altere para o endpoint correto
-    return response.data; // Isso deve retornar a lista de subcategorias
+    const response = await axios.get("http://localhost:8080/subcategorias");
+    return response.data;
   } catch (error) {
     console.error("Erro ao buscar subcategorias:", error);
     return []; // Retorna um array vazio em caso de erro
