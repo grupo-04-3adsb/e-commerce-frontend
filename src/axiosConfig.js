33e221c36bcc76c7019591e03e4d9fbbc6853c2f
Matheus-Munari <142364217+Matheus-Munari@users.noreply.github.com>
2025-06-15 19:41:58 -0300

    Update da base url da api

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
    // "http://localhost:8080",
-  baseURL: "https://tcatelieback-eub2epddgsdfamd3.canadacentral-01.azurewebsites.net",
+  baseURL: "https://tcatelieback-eub2epddgsdfamd3.canadacentral-01.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
